5ce3a444cb7f9b54c73bf190e0dacb617caff56f
Jie Lin <89507.lin@students.itu.edu>
2016-05-07 18:50:49 +0000

    reverse carrier_wave

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,9 +2,9 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       :provider => 'AWS', 
-      :aws_access_key_id => ENV['S3_ACCESS_KEY'], 
-      :aws_secret_access_key => ENV['S3_SECRET_KEY'] 
+      :aws_access_key_id => 'AKIAIRAP5PY7SCA253HQ', 
+      :aws_secret_access_key => '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' 
       }
-    config.fog_directory = ENV['S3_BUCKET']
+    config.fog_directory = 'swe610'
   end
 end
